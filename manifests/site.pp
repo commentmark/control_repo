dccbec8239b74c73b653e216a9d9e2de5e78e96e
commentmark <88890231+commentmark@users.noreply.github.com>
2022-01-15 13:40:31 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,5 @@
 node default {
   file { '/root/READ.me':
     ensure => file,
-    content => 'This is a readme',
-    owner => 'root',
   }
-}
-node 'master.puppet.vm' {
-  include role::master_server
 }
