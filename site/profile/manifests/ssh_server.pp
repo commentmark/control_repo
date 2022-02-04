af6a80dae6e9cf4fb3ef3b513bca830c5f04bebd
commentmark <88890231+commentmark@users.noreply.github.com>
2022-02-04 11:49:34 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDuG3Vhij07FqDqvGM8+H8OxpYfhdoFIX0J0DGsyrSdEh0UBzBkok4gA+oopCLT2T/bqyxfASqDU3Lo3c3CsSmFrcNi8lHsd+ZRbWTS9GGLp1YT1ni79417eDrf2fLX5JObhhCaUXTY172VPADCpp4L28tnGWvL/Bm6DK4muLj2VfUU0WYwB0NAPAbuzjxs8ByaBcmYRe+D6MD8l+j1s/MTuPxE5pMiNmXL+IYCnU6vAqQ6hkC4y5X5AprejvcNucYN7UT8HQ4/sVgInQoSfTOMrhLGnWrDH49q1UYChzWwjPaUk4QzZGu+wh64S7QJTA/mVHULI6ZrSC1oNw0Sdpu7',
 	}  
 }
