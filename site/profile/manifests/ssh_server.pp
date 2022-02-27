9def889af852577cd62ccd73efec695cad06117d
commentmark <88890231+commentmark@users.noreply.github.com>
2022-02-27 05:25:53 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0NVReDpPZmW5DzWPVSgJEJbvSY2o+JNVbepsJguwRX3dsWiCyDgvc6gzYd3unwgjls0d8Cc9FEWaoVOAArwzOAqJvGQLQYXit6EfsoU7Qp56Z1CVgB9ghLlWrRnShwXpxegcwGDIM+pk8PLAZ2Du7AM5bcW+gUjDNK9j6BOF3Bq7iWWy9qGpTqI84V7eOOuVoe60X98kP1JSaegOvvGWqY2UBpt+fThKJsvJk+OMZ9RVJMc65shyP4oY2q2w1HnIJpjfCizj39HRRuLmagKJDTyviVrQ6ndXzF30+NMG41lBVAdzNWBJUl+WeaO7wgIKXWzmvMXAh4h3j1xvQbKnZ',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDRuiDfk6bOHIQPSv/yQSR9N/QwNK29ckeGe/JOjr2c6EyUgyfmtVXBdhUaAhlFwZ57d06E9iD15PZHQ9OUDezc865ojq1lcbPEHcRKwFrczvItYAx8xVDwm0k9TiVNLNmfmy3AXCA+V70QxXc61TcaF+Kc+x4jlTF0kTCmK/v/wiTHD+QGfzVog7i9wyjUHgmFONcCziLxPHtq/UoH9Bg+oCwLWkKq098cndhgACPmxgX4DbdESXI1RfftIgDOz82e5jT8ZFNHNm5EcQ0bQ97U5xaTZarvyFFCnjSRA1rc1mKhDnhlfca68BXOJQnj1XFUB6N0Clvqn4HpBv/PPAYd',
 	}  
 }
